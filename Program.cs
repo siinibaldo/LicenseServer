87e4f1821a610a6624f52ca984f3f080835c4c65
siinibaldo <siinibaldo@gmail.com>
2026-03-22 23:31:54 +0100

    fix env variables loading

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddEnvironmentVariables(); // <-- FIX
 
 // DB SQLite
 builder.Services.AddDbContext<AppDbContext>(options =>
