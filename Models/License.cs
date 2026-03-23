6ceb6a2e12edee3fcb2ecc9088c377afe4d27458
siinibaldo <siinibaldo@gmail.com>
2026-03-23 16:34:29 +0100

    clean working webhook

--- Models/License.cs
+++ Models/License.cs
@@ -3,4 +3,5 @@ public class License
     public int Id { get; set; }
     public string LicenseKey { get; set; } = "";
     public bool IsActive { get; set; }
+    public string? MachineId { get; set; }
 }
